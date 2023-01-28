fd1d6245d568f33dd6f4a6d08d7945e9e6bb761c
Tatyana <tatyana.kurii@mail.ru>
2023-01-28 14:42:33 +0600

    Mission4

---
+++ Mission4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+int InputInt(string massage)
+{
+  System.Console.WriteLine(massage);
+  int x = Convert.ToInt32(Console.ReadLine());
+  return x;
+}
+int a = InputInt("Введите число : ");
+if (a % 7 == 0 && a % 23 == 0)
+System.Console.WriteLine("Да, кратное");
+else
+{
+  System.Console.WriteLine("Нет, не кратное");
+}
